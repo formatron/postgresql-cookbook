874e2c0743dc8ff1b0b27534a5a5397e43a92cc0
Peter Halliday <pghalliday@gmail.com>
2015-12-04 18:32:06 +0100

    ensure the cluster is created

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,12 +3,18 @@ password = node['formatron_postgresql']['postgres_password']
 package 'postgresql'
 package 'postgresql-contrib'
 
-execute "psql -d postgres -c \"ALTER USER postgres WITH PASSWORD '#{password}';\"" do
-  user 'postgres'
-  not_if "PGPASSWORD='#{password}' psql -h localhost -U postgres -d postgres -c '\\l'"
-end
-
 service 'postgresql' do
   supports status: true, restart: true, reload: true
   action [ :enable, :start ]
+end
+
+execute 'Set locale and Create cluster' do
+  command 'export LC_ALL=C; /usr/bin/pg_createcluster --start 9.3 main'
+  action :run
+  not_if { ::File.directory?('/etc/postgresql/9.3/main') }
+end
+
+execute "psql -d postgres -c \"ALTER USER postgres WITH PASSWORD '#{password}';\"" do
+  user 'postgres'
+  not_if "PGPASSWORD='#{password}' psql -h localhost -U postgres -d postgres -c '\\l'"
 end
